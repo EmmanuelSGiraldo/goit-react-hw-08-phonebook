0f4b0b8a5b4670118733270202b1e4dae8ed10c6
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-08-10 17:27:38 -0500

    Updates

--- assets/CreateContact-0cf0bf31.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-f11b9689.js";const r=()=>t.jsx("div",{children:"CreateContact"});export{r as default};
---
+++ assets/CreateContact-ed29e149.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-6dd12f59.js";const r=()=>t.jsx("div",{children:"CreateContact"});export{r as default};
